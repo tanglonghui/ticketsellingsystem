2fe6a00f62e0058f5397e228c3136b796efa29ef
TangLongHui <1776234240qq.com>
2019-03-26 15:43:27 +0800

    大修改

--- ticketsellingsystem.sql
+++ ticketsellingsystem.sql
@@ -10,43 +10,22 @@ Target Server Type    : MYSQL
 Target Server Version : 50525
 File Encoding         : 65001
 
-Date: 2019-03-12 16:02:37
+Date: 2019-03-26 15:41:24
 */
 
 SET FOREIGN_KEY_CHECKS=0;
-
--- ----------------------------
--- Table structure for order
--- ----------------------------
-DROP TABLE IF EXISTS `order`;
-CREATE TABLE `order` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `train_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `price` float NOT NULL,
-  `type` char(255) NOT NULL,
-  `state` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `train_id` (`train_id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `order_ibfk_1` FOREIGN KEY (`train_id`) REFERENCES `train` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `order_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of order
--- ----------------------------
 
 -- ----------------------------
 -- Table structure for pasenger
 -- ----------------------------
 DROP TABLE IF EXISTS `pasenger`;
 CREATE TABLE `pasenger` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `id_card` varchar(255) NOT NULL,
-  `phone` varchar(255) NOT NULL,
-  `type` varchar(255) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `name` char(1) NOT NULL COMMENT '姓名',
+  `id_card` char(1) NOT NULL COMMENT '证件号',
+  `id_card_type` char(1) NOT NULL COMMENT '证件号类型',
+  `phone` int(11) NOT NULL COMMENT '电话',
+  `type` char(1) NOT NULL COMMENT '乘客类型',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -76,17 +55,17 @@ INSERT INTO `test` VALUES ('2', '123');
 DROP TABLE IF EXISTS `train`;
 CREATE TABLE `train` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `train_model` varchar(255) NOT NULL,
-  `start_place` varchar(255) NOT NULL,
-  `end_place` varchar(255) NOT NULL,
-  `start_time` datetime NOT NULL,
-  `end_time` datetime NOT NULL,
-  `first_seats` int(11) NOT NULL,
-  `second_seats` int(11) NOT NULL,
-  `business_seats` int(11) NOT NULL,
-  `frist_price` float NOT NULL,
-  `second_price` float NOT NULL,
-  `business_price` float NOT NULL,
+  `train_card` char(1) NOT NULL COMMENT '火车型号 如 k996',
+  `start_place` varchar(255) NOT NULL COMMENT '起点',
+  `end_place` varchar(255) NOT NULL COMMENT '终点',
+  `start_time` datetime NOT NULL COMMENT '开始时间',
+  `end_time` datetime NOT NULL COMMENT '结束时间',
+  `first_seat` varchar(255) NOT NULL COMMENT '一等座位数',
+  `first_seat_price` int(11) NOT NULL COMMENT '一等座位价格',
+  `second_seat` varchar(255) NOT NULL COMMENT '二等座位数',
+  `second_seat_price` int(11) NOT NULL COMMENT '二等座位数价格',
+  `business_seat` varchar(255) NOT NULL COMMENT '商务座数量',
+  `business_price` int(11) NOT NULL COMMENT '商务座价格',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -99,27 +78,22 @@ CREATE TABLE `train` (
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `account` char(255) NOT NULL,
-  `password` char(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `age` int(11) NOT NULL,
-  `id_card` int(11) NOT NULL,
-  `sex` varchar(255) NOT NULL,
-  `phone` varchar(255) NOT NULL,
-  `type` varchar(255) DEFAULT NULL,
-  `emal` char(255) DEFAULT NULL,
-  `state` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `account` (`account`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `account` char(255) NOT NULL COMMENT '账号',
+  `password` char(255) NOT NULL COMMENT '密码',
+  `name` char(1) NOT NULL COMMENT '姓名',
+  `age` char(1) DEFAULT NULL COMMENT '年龄',
+  `sex` char(255) NOT NULL COMMENT '性别',
+  `id_card` int(11) DEFAULT NULL COMMENT '身份证（证件号）',
+  `id_card_type` char(1) NOT NULL COMMENT '证件类型',
+  `phone` int(11) NOT NULL COMMENT '电话号码',
+  `state` char(255) DEFAULT NULL COMMENT '认证状态',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', '5', '2', '2', '2', '3', '2222', '2', '2', '2', '1');
-INSERT INTO `user` VALUES ('2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '');
-INSERT INTO `user` VALUES ('5', '3', '2', '2', '2', '3', '2', '2', '2', '2', '');
 
 -- ----------------------------
 -- Table structure for user_pasenger
@@ -127,13 +101,31 @@ INSERT INTO `user` VALUES ('5', '3', '2', '2', '2', '3', '2', '2', '2', '2', '')
 DROP TABLE IF EXISTS `user_pasenger`;
 CREATE TABLE `user_pasenger` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `pasenger_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `user_pasenger_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `user_id` int(11) NOT NULL COMMENT '用户id',
+  `pasenger_id` int(11) NOT NULL COMMENT '旅客id',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user_pasenger
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for user_train
+-- ----------------------------
+DROP TABLE IF EXISTS `user_train`;
+CREATE TABLE `user_train` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `train_id` int(11) NOT NULL,
+  `pasenger_id` int(11) NOT NULL,
+  `order_time` datetime NOT NULL COMMENT '订单时间',
+  `seat` varchar(255) NOT NULL COMMENT '座位级别，123',
+  `price` char(10) NOT NULL COMMENT '价格',
+  `state` varchar(255) NOT NULL COMMENT '是否支付',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user_train
 -- ----------------------------
